022ef99729c7508384909a2b910e276400d3bd47
Christopher Gosala <cgosala@deloitte.com>
2018-06-22 15:27:27 +0530

    using parse Int for limit

--- server/services/post-service.js
+++ server/services/post-service.js
@@ -8,73 +8,73 @@ var donationService = require('./donation-service');
 var postCtrl = new BaseCtrl(postModel);
 
 
-postCtrl.get = function(id, cb) {
+postCtrl.get = function (id, cb) {
     this.model
-    .findOne({ _id: id })
-    .populate('postedBy', 'name emailID')
-    .lean()
-    .exec(function (err, post) {
-        if (err) {
-            return cb(err, null);
-        }
-
-        if (post === null) {
-            return cb({
-                'reason': 'post not found'
-            }, null);
-        }
-
-        commentService.fetchPostComments(post._id, (err, comments) => {
+        .findOne({ _id: id })
+        .populate('postedBy', 'name emailID')
+        .lean()
+        .exec(function (err, post) {
             if (err) {
                 return cb(err, null);
             }
 
-            post.comments = comments;
+            if (post === null) {
+                return cb({
+                    'reason': 'post not found'
+                }, null);
+            }
 
-            donationService.fetchPostDonations(post._id, (err, donations) => {
+            commentService.fetchPostComments(post._id, (err, comments) => {
                 if (err) {
                     return cb(err, null);
                 }
 
-                post.donationHistory = donations;
-        
-                post.lastUpdateAt = new Date(post.lastUpdateAt);
-                post.createdAt = new Date(post.createdAt);
-                return cb(null, post);
-                
+                post.comments = comments;
+
+                donationService.fetchPostDonations(post._id, (err, donations) => {
+                    if (err) {
+                        return cb(err, null);
+                    }
+
+                    post.donationHistory = donations;
+
+                    post.lastUpdateAt = new Date(post.lastUpdateAt);
+                    post.createdAt = new Date(post.createdAt);
+                    return cb(null, post);
+
+                });
+
             });
-            
         });
-    });
 };
 
 postCtrl.getLimitedResults = function (cb) {
     this.model.find({})
         .populate('createdBy', 'name emailID')
-        .sort({createdAt: 'desc'})
-        .limit(process.env.FEEDS_LIMIT || 5)
+        .sort({ createdAt: 'desc' })
+        .limit(parseInt(process.env.FEEDS_LIMIT, 10) || 5)
         .lean()
         .exec(function (err, docs) {
-        if (err) {
-            return cb(err, null);
-        }
-        cb(null, docs);
-    });
+            if (err) {
+                return cb(err, null);
+            }
+            cb(null, docs);
+        });
 };
 
 postCtrl.cumulativeFilter = function (filterDetails, cb) {
     filterDetails.title = "/.*" + (filterDetails.title ? filterDetails.title : "") + ".*/";
-    
+
     this.model.find(filterDetails)
         .populate('createdBy', 'name emailID')
-        .sort({createdAt: 'desc'})
+        .sort({ createdAt: 'desc' })
         .lean()
         .exec(function (err, docs) {
-        if (err) {
-            return cb(err, null);
-        }
-        cb(null, docs);
-    });
+            if (err) {
+                return cb(err, null);
+            }
+            cb(null, docs);
+        });
 };
 
 
